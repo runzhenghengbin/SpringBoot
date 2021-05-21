377f657fdb28e3835434d88689e0d33dd48a5f58
郑恒彬 <zhenghengbin@qlteacher.com>
2021-05-21 10:13:18 +0800

    update

--- springboot(十三)/pdf/pdf_demo/src/main/java/com/kevin/pdf_demo/controller/PdfController.java
+++ springboot(十三)/pdf/pdf_demo/src/main/java/com/kevin/pdf_demo/controller/PdfController.java
@@ -7,12 +7,13 @@ import com.itextpdf.tool.xml.XMLWorkerFontProvider;
 import com.itextpdf.tool.xml.XMLWorkerHelper;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
-import org.apache.commons.io.IOUtils;
+import freemarker.template.TemplateException;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.nio.charset.Charset;
@@ -25,35 +26,35 @@ import java.util.Map;
  */
 @RestController
 public class PdfController {
-    @Value("${DEST}")
-    private String dest;
     @Value("${HTML}")
     private String html;
     @Value("${FONT}")
     private String font;
-    private static Configuration freemarkerCfg = null;
+    @Resource
+    private Configuration freemarkerConfig;
 
 
     @RequestMapping(value = "helloPdf")
-    public void showPdf(HttpServletResponse response) throws IOException, DocumentException {
+    public void showPdf(HttpServletResponse response) throws IOException, DocumentException, TemplateException {
         //需要填充的数据
         Map<String, Object> data = new HashMap<>(16);
         data.put("name", "kevin");
 
-        String content = freeMarkerRender(data,html);
+        String content = freeMarkerRender(data, html);
+        response.reset();
+        response.setContentType("application/octet-stream");
+        response.addHeader("Content-Disposition", "attachment;filename=" + new String("filename.pdf".getBytes("gbk"), "ISO8859-1"));
         //创建pdf
-        createPdf(content, dest);
-
-        // 读取pdf并预览
-        readPdf(response);
-
+        ServletOutputStream out = response.getOutputStream();
+        createPdf(content, out);
+        out.flush();
     }
 
-    public  void createPdf(String content,String dest) throws IOException, DocumentException {
+    public void createPdf(String content, OutputStream dest) throws IOException, DocumentException {
         // step 1
         Document document = new Document();
         // step 2
-        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(dest));
+        PdfWriter writer = PdfWriter.getInstance(document, dest);
         // step 3
         document.open();
         // step 4
@@ -69,63 +70,18 @@ public class PdfController {
     /**
      * freemarker渲染html
      */
-    public  String freeMarkerRender(Map<String, Object> data, String htmlTmp) {
+    public String freeMarkerRender(Map<String, Object> data, String htmlTmp) throws IOException, TemplateException {
         Writer out = new StringWriter();
-
         try {
-            // 获取模板,并设置编码方式
-            setFreemarkerCfg();
-            Template template = freemarkerCfg.getTemplate(htmlTmp);
-            template.setEncoding("UTF-8");
+            Template template = freemarkerConfig.getTemplate(htmlTmp);
             //将合并后的数据和模板写入到流中，这里使用的字符流
             template.process(data, out);
             out.flush();
             return out.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
         } finally {
-            try {
-                out.close();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
-        return null;
-    }
-    /**
-     * 设置freemarkerCfg
-     */
-    private void setFreemarkerCfg() {
-        freemarkerCfg = new Configuration();
-        //freemarker的模板目录
-        try {
-            freemarkerCfg.setDirectoryForTemplateLoading(new ClassPathResource("template").getFile());
-        } catch (IOException e) {
-            e.printStackTrace();
+            out.close();
         }
     }
-
-    /**
-     * 读取本地pdf,这里设置的是预览
-     */
-    private void readPdf(HttpServletResponse response) {
-        response.reset();
-        response.setContentType("application/pdf");
-        try {
-            File file = new File(dest);
-            FileInputStream fileInputStream = new FileInputStream(file);
-            OutputStream outputStream = response.getOutputStream();
-            IOUtils.write(IOUtils.toByteArray(fileInputStream), outputStream);
-            response.setHeader("Content-Disposition",
-                    "inline; filename= file");
-            outputStream.flush();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
 
 
 }
